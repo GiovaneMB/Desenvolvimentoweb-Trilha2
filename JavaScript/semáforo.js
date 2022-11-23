481b5e4399aad506f7ad13dc19e2b39de6a85612
Giovane Mendes Barbosa <116196819+GiovaneMB@users.noreply.github.com>
2022-11-23 14:48:41 -0300

    Create semáforo.js

---
+++ JavaScript/semáforo.js
@@ -0,0 +1,9 @@
+ var semaforo = 'amarelo'
+
+if(semaforo =='verde'){
+    console.log(`a cor ${semaforo} e seguro`)
+}else if(semaforo == 'amarelo'){
+    console.log(`a cor ${semaforo} e atençao`)
+}else if (semaforo == 'vermelho'){
+    console.log(`a cor ${semaforo} e parar`)
+}
