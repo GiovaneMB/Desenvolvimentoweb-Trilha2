48c8bd04837a4506934237585d80c34e13961798
Giovane Mendes Barbosa <116196819+GiovaneMB@users.noreply.github.com>
2022-11-23 14:48:36 -0300

    Update Condicionais.js

--- JavaScript/Condicionais.js
+++ JavaScript/Condicionais.js
@@ -1,1 +1,16 @@
-console.log("teste")
+let numero=5
+if(numero<3){
+    console.log("numero menor que 3")
+}else{
+    console.log("numero maior ou igual a 3")
+}
+
+
+let num = 10
+if(num<0){
+    console.log(`o numero ${num} e negativo`)
+}else if(num>0){
+    console.log(`o numero ${num} e possitivo`)
+}else if (num==0){
+    console.log(`o numero ${num} e zero`)
+}
