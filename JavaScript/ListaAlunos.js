969f83ec0a992fb3b4a9e45a2fe43410d037613e
Giovane Mendes Barbosa <116196819+GiovaneMB@users.noreply.github.com>
2022-11-23 15:54:15 -0300

    Create ListaAlunos.js

---
+++ JavaScript/ListaAlunos.js
@@ -0,0 +1,18 @@
+let ListaAlunos = 'Talita'
+switch(ListaAlunos){
+    case 'Giovane':
+        console.log('Giovane tem 23')
+        break
+        case 'Joao':
+           console.log('Joao tem 17')
+            break
+            case 'Antonio':
+           console.log('Antonio tem 17')
+            break
+            case 'Vitoria':
+               console. log('Vitoria tem 17')
+               break
+               default:
+                   console.log('Nome fora da turma')
+                   break
+}
