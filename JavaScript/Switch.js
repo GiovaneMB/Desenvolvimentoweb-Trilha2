dc054ee1bddb4acfef98f4ad7b075c0ec5c5bbd9
Giovane Mendes Barbosa <116196819+GiovaneMB@users.noreply.github.com>
2022-11-23 15:54:25 -0300

    Create Switch.js

---
+++ JavaScript/Switch.js
@@ -0,0 +1,18 @@
+let itemCompra = 'Cenoura'
+switch(itemCompra){
+    case 'Abacate':
+        console.log('Abacate Kg: R$2,99')
+        break
+        case 'Tomate':
+           console.log('Tomate Kg: R$1,99')
+            break
+            case 'Alface':
+           console.log('Alface unidade:R$4,991')
+            break
+            case 'Cenoura':
+               console. log('Cenoura Kg: R$0,99')
+               break
+               default:
+                   console.log('Item invalido')
+                   break
+}
