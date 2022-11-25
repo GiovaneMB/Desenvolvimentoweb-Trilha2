99b773e6558bad829775ea341e08dbd674b81ce3
Giovane Mendes Barbosa <116196819+GiovaneMB@users.noreply.github.com>
2022-11-25 14:43:55 -0300

    Create funçao_exercicios.js

---
+++ JavaScript/funçao_exercicios.js
@@ -0,0 +1,31 @@
+//Funçao que calcula a soma de 2 números.
+function CalcularSoma(n1,n2){
+console.log(n1+n2)
+}
+CalcularSoma(10,10)
+
+//Funçao que calcula a divisão de 2 números.
+function CalcularDivisao(num1,num2){
+    console.log(num1/num2)
+    }
+    CalcularDivisao(10,5)
+
+//Funçao que calcula a área de um triangulo.
+function CalcularATriangulo(base,altura){
+    console.log((base*altura)/2)
+}
+CalcularATriangulo(10,10)
+
+//Funçao que exiba uma mensagem de erro caso a idade seja menor que 18 anos.
+function mensagem(idade){
+    if(idade<18){
+        console.log(`voce e menor de idade`)
+    }else{
+        console.log(`erro: voce e maior de idade`)
+    }
+}
+mensagem(20)
+
+//funçao que mostre sua altura em centímetros.
+function altura(centímetros)
+console.log ()
