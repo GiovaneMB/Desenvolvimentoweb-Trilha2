a3f96df5c0b5d307ea214590ea01e1b65f65f915
Giovane Mendes Barbosa <116196819+GiovaneMB@users.noreply.github.com>
2022-11-30 14:55:19 -0300

    Update funçao_exercicios.js

--- JavaScript/funçao_exercicios.js
+++ JavaScript/funçao_exercicios.js
@@ -27,5 +27,43 @@ function mensagem(idade){
 mensagem(20)
 
 //funçao que mostre sua altura em centímetros.
-function altura(centímetros)
-console.log ()
+
+
+//funçao que mostre sua idade em anos, meses e depois em dias.
+
+function AnosMesesDias(Anos,Meses,Dias){
+Dias=Anos*365
+Meses=Dias/30
+
+console.log(`voce tem ${Anos} de idade`)
+console.log(`voce tem ${Dias} em dias de idade`)
+console.log(`voce tem ${Meses} em meses de idade`)
+}
+AnosMesesDias(23)
+
+//Funçao que mostre o tamanho do nome do usuário.
+
+
+//Crie uma funçao e salve seu resultado em uma variável:
+function calcularArea(altura, largura){
+    console.log(altura*largura)
+}
+//Que calcula a distância entre seu endereço e o local onde está no momento.
+
+//Que calcula a média e a soma de 5 notas.
+function notas(nota1, nota2, nota3, nota4, nota5){
+media=nota1, nota2, nota3, nota4, nota5/5
+console.log(`A Soma das notas e: ${nota1+nota2+nota3+nota4+nota5}`)
+console.log(`A media entre as notas e: ${media}`)
+}
+notas(10,11,12,13,14)
+
+//Que calcula a área de um círculo.
+const raio = 10
+const pi = Math.PI
+let area = 2* pi * raio
+console.log(area)
+
+//Que exiba uma mensagem de erro caso os argumentos sejam diferentes do tipo integer (inteiro) e negativos.
+
+//Que peça sua data de nascimento (ano) e calcule e imprima quantos anos você terá em 2025.
